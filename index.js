1862821c17cd0ef4817a02ba659110d7af79aa76
Abha Mam <apm6283@psu.edu>
2025-01-11 04:16:01 -0500

    reached part 6

--- index.js
+++ index.js
@@ -102,21 +102,28 @@ function filterUnfundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have not yet met their goal
+    let games = GAMES_JSON.filter((game) =>{
+return game.pledged < game.goal
+    });
 
 
     // use the function we previously created to add the unfunded games to the DOM
+    addGamesToPage(games);
 
 }
+
 
 // show only games that are fully funded
 function filterFundedOnly() {
     deleteChildElements(gamesContainer);
 
     // use filter() to get a list of games that have met or exceeded their goal
-
-
+    let games = GAMES_JSON.filter((game) =>{
+        return game.pledged >= game.goal
+            });
+        
     // use the function we previously created to add unfunded games to the DOM
-
+    addGamesToPage(games);
 }
 
 // show all games
@@ -124,6 +131,7 @@ function showAllGames() {
     deleteChildElements(gamesContainer);
 
     // add all games from the JSON data to the DOM
+    addGamesToPage(GAMES_JSON)
 
 }
 
@@ -133,7 +141,9 @@ const fundedBtn = document.getElementById("funded-btn");
 const allBtn = document.getElementById("all-btn");
 
 // add event listeners with the correct functions to each button
-
+unfundedBtn.addEventListener("click", filterUnfundedOnly);
+fundedBtn.addEventListener("click", filterFundedOnly);
+allBtn.addEventListener("click", showAllGames);
 
 /*************************************************************************************
  * Challenge 6: Add more information at the top of the page about the company.
